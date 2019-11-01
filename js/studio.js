b1a78d80163ba1b130fde509465ea908a79109d2
DjCooGie <evansonchagwa01@gmail.com>
2019-11-01 12:36:17 +0300

    added functions to toggle the paragraphs and images in divs; #des, #dev and #pmg

---
+++ js/studio.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+  
+  $("#des").click(function () {
+    $("#des p").toggle();
+    $("#des img").toggle();
+  });
+
+  $("#dev").click(function () {
+    $("#dev p").toggle();
+    $("#dev img").toggle();
+  });
+
+  $("#pmg").click(function () {
+    $("#pmg p").toggle();
+    $("#pmg img").toggle();
+  });
+
+
+});
