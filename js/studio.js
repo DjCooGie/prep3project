a31ea8f87b47ab17d89f419bb45bc493daa9af71
DjCooGie <evansonchagwa01@gmail.com>
2019-11-02 17:34:28 +0300

    added a license file, README.md file and what-we-do-description.md file

--- js/studio.js
+++ js/studio.js
@@ -16,4 +16,24 @@ $(document).ready(function () {
   });
 
 
+
+
+
+  // form function
+
+  // $(".form").submit(function (e) {
+  //   let name = document.getElementById("name");
+  //   let email = document.getElementById("email");
+  //   let message = document.getElementById("sms");
+    
+  //   if (!name.value || !email.value || !message.value) {
+  //     alert("Kindly check your entries")
+  //   } else {
+  //     e.preventDefault()
+  //     $(this).get(0).reset()
+  //     alert("Thank you! Your message has been received.")
+  //   }
+  // });
+
+
 });
